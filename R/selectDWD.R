ba4a2e0c928566694735c34b30328e3cd6098372
brry <berryboessenkool@hotmail.com>
2016-10-22 15:55:59 +0200

    selectDWD: vectorized, arguments standardized

--- R/selectDWD.R
+++ R/selectDWD.R
@@ -1,7 +1,9 @@
 #' Select data from the DWD CDC FTP Server
 #'
 #' Select files for downloading with \code{\link{dataDWD}}.
-#' Each argument can be a vector, then the rules below are applied element-wise.\cr # ToDo: not yet
+#' All arguments (except for \code{index} and \code{base})
+#' can be a vecor and will be recycled to the maximum length of all arguments.
+#' If that length > 1, the output is a list of filenames.\cr
 #' If station \code{name} is given, but \code{id} is empty (""),
 #' \code{id} is inferred via \code{findex}.
 #' # ToDo: fileIndex (instead of indexlist), metaIndex. argument names findex, mindex
@@ -25,50 +27,55 @@
 #' @examples
 #' \dontrun{
 #' selectDWD(id="00386", res="daily", var="kl", time="historical")
+#' selectDWD(id="00386", res="daily", var="kl", time="historical", meta=TRUE)
 #' selectDWD(id="00386")
 #' selectDWD(id="00386", meta=TRUE)
-#' selectDWD(res="daily", var="kl", time="recent")
+#' selectDWD(res="daily", var="kl", time="recent") # id missing, thus meta<-TRUE
 #' selectDWD(var="dummy") # should give an informative error
 #' selectDWD(time="dummy") # this one, too
 #' }
 #'
-#' @param id,name Character strings. If both are "", \code{meta} is set to TRUE.
-#'                If \code{id} is given, \code{name} is ignored. DEFAULT: ""
-#' @param meta Logical: return metadata txt file name instead of climate data zip file?
-#'             DEFAULT: FALSE
-#' @param index Index used to select filename, as returned by
-#'              \code{\link{index2df}}. To use a current / custom index, use
+#' @param name  Char: station name that will be matched in \code{index} to obtain
+#'              \code{id}.  Not yet implemented!  DEFAULT: ""
+#' @param id    Char: station ID with leading zeros, e.g. "00614".
+#'              If \code{id} is given, \code{name} is ignored. DEFAULT: ""
+#' @param base  Single char: main directory of DWD ftp server, defaulting to
+#'              observed climatic records.
+#'              Must be the same \code{base} used to create \code{index}.
+#'              DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/}
+#' @param res   Char: temporal resolution available at \code{base}, usually one of
+#'              \code{c("hourly","daily","monthly")}. DEFAULT: ""
+#' @param var   Char: Weather variable of interest, usually one of
+#'              \code{c("kl", "more_precip", "soil_temperature", "solar",
+#'              "air_temperature", "cloudiness", "precipitation", "pressure", "sun", "wind")}.
+#'              See more in \code{View(rdwd:::\link{indexlist})}. DEFAULT: ""
+#' @param time  Char: desired time range. One of
+#'              "recent" (data from the last year, up to date usually within a few days) or
+#'              "historical" (long time series). DEFAULT: ""
+#'              ToDo: make time abbreviatable
+#' @param index Single object: Index used to select filename, as returned by
+#'              \code{\link{index2df}}.To use a current / custom index, use
 #'              \code{myIndex <- index2df(indexDWD("/daily/solar"))}
 #'              (with desired path, of course). DEFAULT: rdwd:::indexlist
-#' @param base Main directory of DWD ftp server, defaulting to observed climatic records.
-#'             Must be the same \code{base} used to create \code{index}.
-#'             DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/}
-#' @param res Temporal resolution available at \code{base}, usually one of
-#'            \code{c("hourly","daily","monthly")}. DEFAULT: ""
-#' @param var Weather variable of interest, usually one of
-#'            \code{c("kl", "more_precip", "soil_temperature", "solar",
-#'            "air_temperature", "cloudiness", "precipitation", "pressure", "sun", "wind")}.
-#'            See more in \code{\link{indexlist}}. DEFAULT: ""
-#' @param time Desired time range. One of
-#'             "recent" (data from the last year, up to date usually within a few days) or
-#'             "historical" (long time series). DEFAULT: ""
-#' @param files Logical for case 3/4 with \code{meta}=TRUE: istead of station metadata,
-#'              return a list of the currently available files?
+#' @param meta  Logical: return metadata txt file name instead of climate data zip file?
+#'              Relevant only in case 4 (path and id given). DEFAULT: FALSE
+#' @param files Logical: for case 3/4 with given \code{path}: instead of station
+#'              metadata, return a list of the currently available files?
 #'              This will call code{\link{indexDWD}} with the path, thus
-#'              requiring the \code{RCurl} package.  DEFAULT: FALSE
+#'              requiring the \code{RCurl} package. DEFAULT: FALSE
 #' @param ziponly Logical: If \code{files=TRUE}, only return the zip files, not the
-#'                description (Beschreibung.txt/.pdf) files? DEFAULT: TRUE
+#'              description (Beschreibung.txt/.pdf) files? DEFAULT: TRUE
 #' @param \dots Further arguments passed to \code{\link{indexDWD}} if \code{files=TRUE}
 #'
 selectDWD <- function(
-id="",
 name="",
-meta=FALSE,
-index=rdwd:::indexlist,
+id="",
 base="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate",
 res="",
 var="",
 time="",
+index=rdwd:::indexlist,
+meta=FALSE,
 files=FALSE,
 ziponly=TRUE,
 ...
@@ -76,63 +83,83 @@ ziponly=TRUE,
 {
 # Input checks and processing:
 indexname <- deparse(substitute(index))
-id   <-   id[1]
-name <- name[1]
-meta <- meta[1]
-base <- base[1] # toDo: recycle all to max length + lapply + progress bar
-res  <-  res[1]
-var  <-  var[1]
-time <- time[1]
+len <- max(length(name), length(id), length(res), length(var), length(time)  )
+# recycle input vectors
+if(len>1)
+  {
+  name    <- rep(name,   length.out=len)
+  id      <- rep(id,     length.out=len)
+  res     <- rep(res,    length.out=len)
+  var     <- rep(var,    length.out=len)
+  time    <- rep(time,   length.out=len)
+  meta    <- rep(meta,   length.out=len)
+  files   <- rep(files,  length.out=len)
+  ziponly <- rep(ziponly,length.out=len)
+  }
+# be safe for accidental vector input
+base <- base[1]
+# loop over each input element:
+output <- lapply(seq_len(len), function(i)
+{
 # infer id from name if id=="" and name!="":
 # ToDo
 # cases (as in description)
-givenid <- id!=""
-givenpath <- res!="" & var!="" # ignore time, because of var=solar possibility (no time)
+givenid <- id[i]!=""
+givenpath <- res[i]!="" & var[i]!="" # ignore time, because of var=solar possibility (no time)
 # 1: id and path are both empty ------------------------------------------------
 if(!givenid & !givenpath)
   {
-  stop("Neither station ID nor FTP folder is given.")   # ToDo: decide what to do.
-  }                                                     # Maybe some result from metaIndex
+  stop("in selectDWD: Neither station ID nor FTP folder is given.", call.=FALSE)
+  # call.=FALSE to avoid uninformative  Error in FUN(X[[i]], ...) :
+  # ToDo: decide what to do. Maybe some result from metaIndex
+  }
 # 2: id given, path empty ------------------------------------------------------
 if(givenid & !givenpath)
   {
-  if(meta) warning("meta is currently ignored if id is given") #ToDo: decide what to do.
-  filename <- index[id==index$id,"path"]                       # Maybe some result from metaIndex
+  if(meta) warning("selectDWD: meta is currently ignored if id is given", call.=FALSE)
+  #ToDo: decide if there should instead of ignoring be some result from metaIndex
+  filename <- index[id[i]==index$id,"path"]
   return(   paste0(base, filename)   )
   }
 #
 # Case 3 and 4 (path given) - path existence check -----------------------------
-if(var=="solar") time <- ""
-path <- paste0("/",res,"/",var,"/",time)
-if(all(!grepl(path, index$path))) stop("According to index '",indexname,
-      "', the path '", path,"' doesn't exist. See ?metaDWD on how to use a different index.")
+time.i <- time[i]
+if(var[i]=="solar") time.i <- ""
+path <- paste0("/",res[i],"/",var[i],"/",time.i)
+if(all(!grepl(path, index$path))) stop("in selectDWD: According to index '",
+       indexname, "', the path '", path,
+       "' doesn't exist. See ?metaDWD on how to use a different index.", call.=FALSE)
 # select entry from index:
-sel <- index$res==res & index$var==var & index$time==time
+sel <- res[i]==index$res & var[i]==index$var & time.i==index$time
 # # list available files:
-if(files) return(indexDWD(path, base=base, ziponly=ziponly, ...))
+if(files[i]) return(indexDWD(path, base=base, ziponly=ziponly[i], ...))
 #
 # 3: id is empty, path is given ------------------------------------------------
-if(!givenid & givenpath) meta <- TRUE
+meta.i <- meta[i]
+if(!givenid & givenpath) meta.i <- TRUE
 # if either   case 3   or   4 with meta=TRUE  : return name of description txt file
-if(meta)
+if(meta.i)
   {
   sel <- sel & substr(index$path, nchar(index$path)-3, 1e4)==".txt"
   sel <- sel & grepl("Beschreibung", index$path)
   # checks:
-  if(sum(sel)==0) stop("According to index '",indexname,
+  if(sum(sel)==0) stop("in selectDWD: According to index '",indexname,
                      "', there is no description file in '", path,
-                     "'. See ?metaDWD on how to use a different index.")
+                     "'. See ?metaDWD on how to use a different index.", call.=FALSE)
   }
 # 4: id and path are both given ------------------------------------------------
 if(givenid & givenpath)
   {
-  sel <- sel & id==index$id
-  if(sum(sel)==0) stop("According to index '",indexname,
+  if(!meta.i) sel <- sel & id[i]==index$id
+  if(sum(sel)==0) stop("in selectDWD: According to index '",indexname,
                        "', there is no file in '", path, "' with id '",id,
-                       "'. See ?metaDWD on how to use a different index.")
+                       "'. See ?metaDWD on how to use a different index.", call.=FALSE)
   }
 filename <- index[sel,"path"]
-if(length(filename)!=1) stop("Several (or no) files were selected: ", toString(filename))
+if(length(filename)!=1) stop("in selectDWD: Several (or no) files were selected: ",
+                             toString(filename), call.=FALSE)
 return(   paste0(base, filename)   )
-# toDo: loop end stuff
+}) # loop end
+output <- if(len==1) output[[1]] else output
+return(output)
 }
