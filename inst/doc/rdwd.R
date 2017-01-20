ffab0fcb26d69647fb458f02820e571c5cbc36dc
brry <berryboessenkool@hotmail.com>
2017-01-20 14:15:56 +0100

    vignette: initial release with package structure and interactive map

---
+++ inst/doc/rdwd.R
@@ -0,0 +1,14 @@
+## ----install, eval=FALSE-------------------------------------------------
+#  install.packages("rdwd")
+#  # development version from github:
+#  berryFunctions::instGit("brry/rdwd")
+
+## ----library-------------------------------------------------------------
+library(rdwd)
+
+## ----map, fig.height=7, fig.width=7, warning=FALSE-----------------------
+library(leaflet)  ;  data(mapDWD)  ;  mapDWD
+
+## ----example, fig.show='hold', echo=-3-----------------------------------
+#Hi
+
