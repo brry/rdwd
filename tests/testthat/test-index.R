270d2d05780893e5701e3983ecf510d15ef2bcd7
brry <berryboessenkool@hotmail.com>
2017-01-21 23:07:53 +0100

    map stuff moved to test file for now. Might change into a third vignette...

--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -43,6 +43,55 @@ checkdupli("name", "id", geoIndex) # 44 with 2
 sum(geoIndex$nfiles_coord) # 25482
 hist(geoIndex$nfiles_coord, breaks=100, col="cadetblue", xlab="number of files per location")
 
+
+# static maps -----
+
+if(!requireNameSpace("OSMscale")) install.packages("OSMscale")
+library("OSMscale")
+
+# Map of all precipitation stations (metaindex):
+map <- pointsMap(geoBreite, geoLaenge, data=metaIndex, fx=0.28, fy=0.06)
+pdf("DWDdata/RainfallStationsMap.pdf")
+plot(map)
+scaleBar(map, x=0.05, y=0.03, abslen=200)
+pp <- projectPoints(geoBreite, geoLaenge, data=metaIndex, to=posm())
+points(pp[!metaIndex$hasfile,], col="red", pch=3)
+points(pp[ metaIndex$hasfile,], col="blue", pch=3)
+legend("bottomright", c("in matadata only", "file on FTP server"),
+       col=c("red", "blue"), pch=3, bg="white")
+title(main="DWD stations: data on ftp server", line=3)
+dev.off()
+
+
+# map geoIndex
+
+map <- pointsMap(lat, long, data=geoIndex, fx=0.06, fy=0.06)
+pdf("DWDdata/RainfallStationsMap_nfiles.pdf", width=5)
+plot(map)
+scaleBar(map, x=0.05, y=0.03, abslen=200)
+geoIndex <- sortDF(geoIndex, "nfiles_coord", decreasing=FALSE)
+pp <- projectPoints(lat, long, data=geoIndex, to=posm())
+points(pp, cex=0.6)
+colPoints(pp$x, pp$y, geoIndex$nfiles_coord, cex=0.6, zlab="")
+title(main="DWD stations: number of files on ftp server", line=3)
+dev.off()
+
+
+# Time series duration:
+# colPoints <- berryFunctions::colPoints
+colPoints(geoLaenge, geoBreite, Stations_id, data=metaIndex, add=F, asp=1.5)
+colPoints(geoLaenge, geoBreite, Stationshoehe, data=metaIndex, add=F, asp=1.5)
+metaIndex$von_jahr <- metaIndex$von_datum/1e4
+metaIndex$bis_jahr <- metaIndex$bis_datum/1e4
+metaIndex$dauer <- metaIndex$bis_jahr - metaIndex$von_jahr
+colPoints(geoLaenge, geoBreite, von_jahr, data=metaIndex, add=F, asp=1.5)
+colPoints(geoLaenge, geoBreite, bis_jahr, data=metaIndex, add=F, asp=1.5)
+colPoints(geoLaenge, geoBreite, dauer, data=metaIndex, add=F, asp=1.5)
+hist(metaIndex$bis_jahr, breaks=50, col="purple")
+hist(metaIndex$dauer, breaks=50, col="purple")
+sum(metaIndex$dauer>50); mean(metaIndex$dauer>50)
+# 45% of stations with more than 50 years of data (according to metadata)
+
 }
 
 
