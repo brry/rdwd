brry/rdwd

commit 47d6c0d180fa6fbdba7f38688fc23f000889fa91

dwdbase: central package base URL created

R/dataDWD.R
@@ -79,7 +79,8 @@
 #' @param file   Char (vector): complete file URL(s) (including base and filename.zip) as returned by
 #'               \code{\link{selectDWD}}. Can be a vector with several filenames.
 #' @param base   Single char: base URL that will be removed from output file names.
-#'               DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
+#'               DEFAULT: \code{\link{dwdbase}}:
+#'               \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
 #' @param dir    Char: Writeable directory name where to save the downloaded file.
 #'               Created if not existent. DEFAULT: "DWDdata" at current \code{\link{getwd}()}
 #' @param force  Logical (vector): always download, even if the file already exists in \code{dir}?
@@ -111,7 +112,7 @@
 #
 dataDWD <- function(
 file,
-base="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate/",
+base=dwdbase,
 dir="DWDdata",
 force=FALSE,
 overwrite=FALSE,

.gitignore
@@ -5,3 +5,4 @@ Meta
 .RData
 testfread/
 inst/shinyapps/
+localtests/radolan

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: rdwd
 Title: Select and Download Climate Data from 'DWD' (German Weather Service)
-Version: 0.11.3
-Date: 2019-02-11
+Version: 0.11.4
+Date: 2019-02-12
 Depends: R(>= 2.10)
 SystemRequirements: Pandoc (>= 1.12.3), pandoc-citeproc
 Imports: berryFunctions (>= 1.17.0), pbapply

NAMESPACE
@@ -2,6 +2,7 @@
 
 export(createIndex)
 export(dataDWD)
+export(dwdbase)
 export(findID)
 export(indexFTP)
 export(metaInfo)

R/createIndex.R
@@ -45,7 +45,8 @@
 #' @param paths Char: vector of DWD paths returned by \code{\link{indexFTP}} called
 #'              with the same \code{base} value as this function
 #' @param base  Main directory of DWD ftp server, defaulting to observed climatic records.
-#'              DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
+#'              DEFAULT: \code{\link{dwdbase}}:
+#'              \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
 #' @param dir   Char: writeable directory name where to save the main output(s).
 #'              Created if not existent. DEFAULT: "DWDdata" at current \code{\link{getwd}()}
 #' @param fname Char: Name of file in \code{dir} in which to write \code{\link{fileIndex}}.
@@ -65,7 +66,7 @@
 #' 
 createIndex <- function(
 paths,
-base="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate",
+base=dwdbase,
 dir="DWDdata",
 fname="fileIndex.txt",
 meta=FALSE,

R/indexFTP.R
@@ -41,8 +41,9 @@
 #'                is the default, code{folder} is changed to all folders in current 
 #'                \code{\link{fileIndex}}: \code{unique(dirname(fileIndex$path))}. 
 #'                DEFAULT: "currentfindex"
-#' @param base    Main directory of DWD ftp server, defaulting to observed climatic records.
-#'                DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
+#' @param base    Main directory of FTP server, defaulting to DWD observed climatic records.
+#'                DEFAULT: \code{\link{dwdbase}}:
+#'                \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
 #' @param is.file.if.has.dot Logical: if some of the input paths contain a dot, 
 #'                treat those as files, i.e. do not try to read those as if they
 #'                were a folder. Only set this to FALSE if you know what you're doing.
@@ -66,7 +67,7 @@
 #' 
 indexFTP <- function(
 folder="currentfindex",
-base="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate",
+base=dwdbase,
 is.file.if.has.dot=TRUE,
 sleep=0,
 dir="DWDdata",
@@ -82,9 +83,9 @@ if(!requireNamespace("RCurl", quietly=TRUE))
   stop("The R package 'RCurl' is not available. rdwd::indexFTP can not obtain file list.\n",
        "install.packages('RCurl')       to enable this.")
 # change folder:
-if(all(folder=="currentfindex") & base=="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate")
+if(all(folder=="currentfindex") & base==dwdbase)
    folder <- unique(dirname(fileIndex$path))
-if(base!="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate")
+if(base!=dwdbase)
  if(missing(folder)) warning('base is not the rdwd default. It is likely you want',
                              ' to use folder="" instead of "',folder,'".')
 # Progress bar?

R/rdwd-package.R
@@ -2,6 +2,7 @@
 # Release questions, code for meta-info related functions, index updating
 
 # rdwd-package
+# dwdbase
 # release_questions
 # fileIndex, metaIndex, geoIndex
 # metaInfo
@@ -38,6 +39,16 @@
 #' Deutscher Wetterdienst R Daten download Klimastationen\cr
 #' DWD Daten mit R runterladen, Wetter und Klimadaten in R
 NULL
+
+
+
+# dwdbase ----
+
+#' @title DWD FTP Server base URL
+#' @export
+#' @description base URL to DWD FTP Server / observed climatic records.\cr
+#'     \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
+dwdbase <- "ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate"
 
 
 

R/selectDWD.R
@@ -122,7 +122,7 @@
 #'              Is internally converted to an integer, because some DWD meta data
 #'              files also contain no leading zeros. DEFAULT: findID(name)
 #' @param base  Single char: main directory of DWD ftp server, defaulting to
-#'              observed climatic records.
+#'              observed climatic records (\code{\link{dwdbase}}).
 #'              Must be the same \code{base} used to create \code{findex}.
 #'              DEFAULT: \url{ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate}
 #' @param res   Char: temporal \bold{res}olution available at \code{base}, usually one of
@@ -164,7 +164,7 @@ name="",
 exactmatch=TRUE,
 mindex=metaIndex,
 id=findID(name, exactmatch=exactmatch, mindex=mindex),
-base="ftp://ftp-cdc.dwd.de/pub/CDC/observations_germany/climate",
+base=dwdbase,
 res="",
 var="",
 per="",

commit a981f2a0331ad93096f47544635d3a6a1f632c21

dataDWD: filenames no longer start with underscore (bug introduced with dwdbase in 47d6c0d180fa6fbdba7f38688fc23f000889fa91)

R/dataDWD.R
@@ -156,7 +156,7 @@ if(browse)
 owd <- dirDWD(dir, quiet=quiet)
 on.exit(setwd(owd))
 # output file name(s)
-outfile <- gsub(base, "", file)
+outfile <- gsub(paste0(base,"/"), "", file)
 outfile <- gsub("/", "_", outfile)
 
 # force=NA management