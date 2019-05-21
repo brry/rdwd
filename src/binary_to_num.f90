cf38225fe8987c7f7a96108ce260c8d12fd7f2f6
brry <berryboessenkool@hotmail.com>
2019-05-21 11:22:46 +0200

    binary_to_num FORTRAN routine simplified

--- src/binary_to_num.f90
+++ src/binary_to_num.f90
@@ -6,7 +6,7 @@
 !            raw --> 2 Byte Dataset (as vector)
 !            numeric --> integer Dataset for output
 
-! INFO: Bit 13 --> Flag for interpolated
+! INFO: Bit 13 --> Flag for interpolated, ignored here
 !       Bit 14 --> Flag for missing
 !       Bit 15 --> Flag for negative
 !       Bit 16 --> Flag for clutter
@@ -19,36 +19,29 @@ subroutine binary_to_num(raw,dims,numeric,fNAval,fCLUTTERval)
   integer,DIMENSION(2),INTENT(in)::dims
   integer(KIND=2),DIMENSION(dims(1)*dims(2)),INTENT(in)::raw
   integer,DIMENSION(dims(1)*dims(2)),INTENT(out)::numeric
-  logical::interp,negative,missing,clutter
   
   ! do cycle over all entries 
-  do i=1,dims(1)*dims(2) 
-
-     interp=.FALSE. ! set interp false before checking
-     missing=.FALSE. ! set missing false before checking
-     negative=.FALSE. ! set negative false before checking
-     clutter=.FALSE. ! set clutter false before checking
-
+  do i=1,dims(1)*dims(2)
+  
      numeric(i)=IBITS(raw(i),0,12)
-     if(IBITS(raw(i),12,1)==1) THEN ! check for interpolated
-        interp=.TRUE.
-     end if
+     
      if(IBITS(raw(i),13,1)==1) THEN ! check for missing 
-        missing=.TRUE.
         numeric(i)=fNAval
      end if
+     
      if(IBITS(raw(i),14,1)==1) THEN ! check for negative
-        negative=.TRUE.
         numeric(i)=numeric(i)*(-1)
      end if
+     
      if(IBITS(raw(i),15,1)==1) THEN ! check for clutter
-        clutter=.TRUE.
         numeric(i)=fCLUTTERval
      end if
      
   end do
 
 end subroutine binary_to_num
+
+
 
 ! INFO: RX only one Byte 
 ! NA-Value: 250
@@ -67,10 +60,12 @@ subroutine binary_to_num_RX(raw,dims,numeric,fNAval,fCLUTTERval)
   do i=1,dims(1)*dims(2) 
 
      numeric(i)=IBITS(raw(i),0,7)
-     if(numeric(i)==250) THEN
+     
+     if(numeric(i)==250) THEN ! check for missing 
         numeric(i)=fNAval
      end if
-     if(numeric(i)==249) THEN
+     
+     if(numeric(i)==249) THEN ! check for clutter 
         numeric(i)=fCLUTTERval
      end if
      
