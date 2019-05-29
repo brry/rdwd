6a2b073c2c000f0a3be6c079567ed45020a31a37
Berry Boessenkool <berry-b@gmx.de>
2019-05-29 22:49:07 +0200

    Revert "Fortran help"

--- src/binary_to_num.f90
+++ src/binary_to_num.f90
@@ -1,18 +1,7 @@
-!-------------------------------------------------------------------------------
 ! FORTRAN subroutine for converting Dataset of 2 Byte raw data to integer
-! Originally written by Henning Rust <henning.rust@met.fu-berlin.de>
-! and Christoph Ritschel (2016). Added to rdwd in May 2019 for readRadarFile
-! used in readDWD.binary. Updated and tweaked by Avraham Adler 2019-05-27
-!-------------------------------------------------------------------------------
+! Originally written by Henning Rust <henning.rust@met.fu-berlin.de> and Christoph Ritschel (2016)
+! Added to rdwd in May 2019 for readRadarFile used in readDWD.binary.
 
-module B2N
-  use, intrinsic :: iso_c_binding
-  implicit none
-  public :: binary_to_num, binary_to_num_rx
-  
-contains
-
-!-------------------------------------------------------------------------------
 ! Parameter: dims --> Dimension of 2 Byte Dataset
 !            raw --> 2 Byte Dataset (as vector)
 !            numeric --> integer Dataset for output
@@ -21,57 +10,65 @@ contains
 !       Bit 14 --> Flag for missing
 !       Bit 15 --> Flag for negative
 !       Bit 16 --> Flag for clutter
-!       Bits 1-12 --> Data Value
-!-------------------------------------------------------------------------------
+!      Bits 1-12 --> Data Value
 
-  subroutine binary_to_num(raw, Flength, numeric, Fna, Fclutter) &
-                           bind(C, name = "binary_to_num_")
-    integer(kind = c_int), intent(in)                   ::Flength, Fna, Fclutter
-    integer(kind = c_int), intent(in), dimension(Flength) ::raw
-    integer(kind = c_int), intent(out), dimension(Flength)::numeric
-    integer                                               ::i
-    
-    ! do cycle over all entries 
-    do i = 1, Flength
-       numeric(i) = IBITS(raw(i), 0, 12)
-       if (IBITS(raw(i), 13, 1) == 1) then ! check for missing 
-          numeric(i) = Fna
-       end if
-       if (IBITS(raw(i), 14, 1) == 1) THEN ! check for negative
-          numeric(i) = -numeric(i)
-       end if
-       if (IBITS(raw(i), 15, 1) == 1) THEN ! check for clutter
-          numeric(i) = Fclutter
-       end if
-    end do
+subroutine binary_to_num(raw,Flength,numeric,Fna,Fclutter)
+  implicit none
+  integer:: i
+  integer,INTENT(in):: Flength,Fna,Fclutter
+  integer(KIND=2),DIMENSION(Flength),INTENT(in)::raw
+  integer,DIMENSION(Flength),INTENT(out)::numeric
   
-  end subroutine binary_to_num
+  ! do cycle over all entries 
+  do i=1,Flength
+  
+     numeric(i)=IBITS(raw(i),0,12)
+     
+     if(IBITS(raw(i),13,1)==1) THEN ! check for missing 
+        numeric(i)=Fna
+     end if
+     
+     if(IBITS(raw(i),14,1)==1) THEN ! check for negative
+        numeric(i)=numeric(i)*(-1)
+     end if
+     
+     if(IBITS(raw(i),15,1)==1) THEN ! check for clutter
+        numeric(i)=Fclutter
+     end if
+     
+  end do
 
-!-------------------------------------------------------------------------------
+end subroutine binary_to_num
+
+
+
+
 ! INFO: RX only one Byte 
 ! NA-Value: 250
 ! Clutter-Value: 249
-!-------------------------------------------------------------------------------
 
-  subroutine binary_to_num_rx(raw, Flength, numeric, Fna, Fclutter) &
-                           bind(C, name = "binary_to_num_rx_")
-    
-    integer(kind = c_int), intent(in)                   ::Flength, Fna, Fclutter
-    integer(kind = c_int), intent(in), dimension(Flength) ::raw
-    integer(kind = c_int), intent(out), dimension(Flength)::numeric
-    integer                                               ::i
- 
-    ! do cycle over all entries 
-    do i=1, Flength
-       numeric(i) = IBITS(raw(i), 0, 7)
-       if (numeric(i) == 250_c_int) then ! check for missing 
-          numeric(i) = Fna
-       end if
-       if (numeric(i) == 249_c_int) then ! check for clutter 
-          numeric(i) = Fclutter
-       end if
-    end do
+subroutine binary_to_num_rx(raw,Flength,numeric,Fna,Fclutter)
+  implicit none
+  integer:: i
+  integer,INTENT(in):: Flength,Fna,Fclutter
+  integer(KIND=1),DIMENSION(Flength),INTENT(in)::raw
+  integer,DIMENSION(Flength),INTENT(out)::numeric
+  logical::interp,negative,missing,clutter
   
-  end subroutine binary_to_num_rx
-  
-end module B2N
+  ! do cycle over all entries 
+  do i=1,Flength 
+
+     numeric(i)=IBITS(raw(i),0,7)
+     
+     if(numeric(i)==250) THEN ! check for missing 
+        numeric(i)=Fna
+     end if
+     
+     if(numeric(i)==249) THEN ! check for clutter 
+        numeric(i)=Fclutter
+     end if
+     
+  end do
+
+end subroutine binary_to_num_rx
+
--- src/init.c
+++ src/init.c
@@ -1,46 +1,21 @@
-#include <R.h>
-#include <Rinternals.h>
+#include <R_ext/RS.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-// Insert Copyright here
+/* Kept as generated with tools::package_native_routine_registration_skeleton(".", "src/init.c")*/
 
-// C-calling-Fortran
+/* .Fortran calls */
+extern void F77_NAME(binary_to_num)(void *, void *, void *, void *, void *);
+extern void F77_NAME(binary_to_num_rx)(void *, void *, void *, void *, void *);
 
-extern void F77_NAME(binary_to_num)(int *raw, int *Flength, int *ret,
-                                    int *Fna, int *Fclutter);
-                                    
-extern void F77_NAME(binary_to_num_rx)(int *raw, int *Flength, int *ret,
-                                       int *Fna, int *Fclutter);
-
-// R-calling-C
-
-extern SEXP B2N_C(SEXP raw, SEXP Flength, SEXP Fna, SEXP Fclutter) {
-    SEXP ret;
-    PROTECT(ret = allocVector(INTSXP, INTEGER(Flength)[0]));
-    F77_CALL(binary_to_num)(INTEGER(raw), INTEGER(Flength), INTEGER(ret),
-             INTEGER(Fna), INTEGER(Fclutter));
-    UNPROTECT(1);
-    return(ret);
-}
-
-extern SEXP B2NRX_C(SEXP raw, SEXP Flength, SEXP Fna, SEXP Fclutter) {
-    SEXP ret;
-    PROTECT(ret = allocVector(INTSXP, INTEGER(Flength)[0]));
-    F77_CALL(binary_to_num_rx)(INTEGER(raw), INTEGER(Flength), INTEGER(ret),
-             INTEGER(Fna), INTEGER(Fclutter));
-    UNPROTECT(1);
-    return(ret);
-}
-
-// Methods & Registration
-static const R_CallMethodDef CallEntries[] = {
-    {"B2N_C",   (DL_FUNC) &B2N_C,   4},
-    {"B2NRX_C", (DL_FUNC) &B2N_C,   4},
-    {NULL,      NULL,               0}
+static const R_FortranMethodDef FortranEntries[] = {
+    {"binary_to_num",    (DL_FUNC) &F77_NAME(binary_to_num),    5},
+    {"binary_to_num_rx", (DL_FUNC) &F77_NAME(binary_to_num_rx), 5},
+    {NULL, NULL, 0}
 };
 
-void R_init_rdwd(DllInfo *dll) {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+void R_init_rdwd(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
