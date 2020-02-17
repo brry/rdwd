8ba3754c17d6ca7154bc9802ce5d8dea2ac94d57
brry <berryboessenkool@hotmail.com>
2020-02-17 13:29:17 +0100

    addBorders: DEU + EUR moved to inst/extdata to handle CRAN build+check on systems without sp installed

--- R/addBorders.R
+++ R/addBorders.R
@@ -20,13 +20,15 @@
 #' @param \dots   Further arguments passed to \code{raster::\link[raster]{plot}}
 addBorders <- function(de="grey80", eu="black", add=TRUE, ...)
 {
-checkSuggestedPackage("raster", "addBorders")
-data("DEU","EUR", envir=environment())
+checkSuggestedPackage("raster", "addBorders") # stops also if sp is missing
+load(system.file("extdata/DEU.rda", package="rdwd"), envir=environment())
+load(system.file("extdata/EUR.rda", package="rdwd"), envir=environment())
 raster::plot(DEU, add=add, border=de, ...) 
 raster::plot(EUR, add=TRUE, border=eu, ...)
 return(invisible(list(DEU=DEU, EUR=EUR)))
 }
-
+# Suppress CRAN check note 'no visible binding for global variable':
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("DEU", "EUR"))
 
 
 # DEU Map dataset --------------------------------------------------------------
@@ -39,14 +41,15 @@ return(invisible(list(DEU=DEU, EUR=EUR)))
 #' \code{DEU <- rgeos::gSimplify(DEU1, tol=0.02, topologyPreserve=FALSE)}\cr
 #' \code{raster::plot(DEU1)}\cr
 #' \code{raster::plot(DEU)}\cr
-#' \code{save(DEU,        file="data/DEU.rda")}\cr
-#' \code{tools::resaveRdaFiles("data/DEU.rda")}\cr
+#' \code{save(DEU,        file="inst/extdata/DEU.rda")}\cr
+#' \code{tools::resaveRdaFiles("inst/extdata/DEU.rda")}\cr
 #' @docType data
 #' @format Formal class 'SpatialPolygons' [package "sp"] with 4 slots
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, May 2018
 #' @keywords datasets
 if(requireNamespace("sp", quietly=TRUE))
-data(DEU, envir=environment())
+load(system.file("extdata/DEU.rda", package="rdwd"), envir=environment())
+# To have it avaliable as rdwd:::DEU
 
 
 # EUR Map dataset --------------------------------------------------------------
@@ -58,12 +61,12 @@ data(DEU, envir=environment())
 #' \code{EUR <- rworldmap::getMap("low")}\cr
 #' \code{EUR <- raster::crop(EUR, c(-5,20, 40,60)) }\cr
 #' \code{raster::plot(EUR)}\cr
-#' \code{save(EUR,        file="data/EUR.rda", version=2)}\cr
-#' \code{tools::resaveRdaFiles("data/EUR.rda", version=2)}\cr
+#' \code{save(EUR,        file="inst/extdata/EUR.rda", version=2)}\cr
+#' \code{tools::resaveRdaFiles("inst/extdata/EUR.rda", version=2)}\cr
 #' @docType data
 #' @format SpatialPolygonsDataFrame [package "sp"] with 32 rows
 #' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Aug 2019
 #' @keywords datasets
 if(requireNamespace("sp", quietly=TRUE))
-data(EUR, envir=environment())
+load(system.file("extdata/EUR.rda", package="rdwd"), envir=environment())
 
