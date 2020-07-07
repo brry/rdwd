d321cdebb06e57fcf83d244edf409f750f515620
brry <berryboessenkool@hotmail.com>
2020-07-07 12:15:59 +0200

    index documentation in own file. misc stuff moved from rdwd-package.R to zzz.R

---
+++ R/index.R
@@ -0,0 +1,59 @@
+# fileIndex, metaIndex, geoIndex, gridIndex, formatIndex ----
+
+#' Indexes of files and metadata on the DWD CDC FTP server
+#' 
+#' Created with \code{\link{indexFTP}} and \code{\link{createIndex}} used in \code{\link{updateIndexes}}.\cr
+#' In functions, you can access them with `rdwd:::fileIndex` etc.\cr
+#' **fileIndex**: A data.frame with the filenames (and derived information)
+#' at the default `base` value \code{\link{dwdbase}}.\cr
+#' **metaIndex**: A data.frame with the contents of all the station description files
+#' (..._Beschreibung_Stationen.txt) under \code{\link{dwdbase}}.\cr
+#' **geoIndex**: `metaIndex` distilled to geographic locations.\cr
+#' **gridIndex**: Vector of file paths at \code{\link{gridbase}}.\cr
+#' **formatIndex**: (modified) table from
+#' <ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/subdaily/standard_format/formate_kl.html>\cr
+#' 
+#' @name index
+#' @aliases fileIndex metaIndex geoIndex gridIndex formatIndex
+#' @docType data
+#' @format
+#' **fileIndex**: data.frame with character strings. ca 260k rows x 8 columns:\cr
+#'         `res`, `var`, `per` (see \code{\link{selectDWD}}),
+#'         station `id`, time series `start` and `end`, and
+#'         `ismeta` information, all according to `path`.\cr
+#' **metaIndex**: data.frame with ca 97k rows for 12 columns:\cr
+#'         `Stations_id, von_datum, bis_datum,
+#'         Stationshoehe, geoBreite, geoLaenge, Stationsname, Bundesland,
+#'         res, var, per, hasfile` \cr
+#' **geoIndex**: data.frame with ca 6k rows for 11 columns:\cr
+#'         `id, name, state, lat, lon, ele, nfiles, nonpublic, recentfile, display, col`\cr
+#' **gridIndex**: Vector with ca 50k file paths at \code{\link{gridbase}}\cr
+#' **formatIndex**: data.frame with 140 rows for 12 columns:\cr
+#'         `Ke_Ind, Kennung, Label, Beschreibung, Einheit, Code-Tabellen,
+#'         Zusatzinfo, Typ, Pos, Erlaubt, Fehlk, dividebyten`\cr
+#' @source Deutscher WetterDienst / Climate Data Center  FTP Server
+#' @seealso \code{\link{createIndex}}, \code{\link{indexFTP}}, \code{\link{selectDWD}},
+#'          \code{\link{findID}}, \code{\link{metaInfo}},
+#'          <https://bookdown.org/brry/rdwd>
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, June-Nov 2016, June 2017, Oct 2019
+#' @keywords datasets
+#' @importFrom utils data
+#' @examples
+#' data(fileIndex)
+#' data(metaIndex)
+#' data(geoIndex)
+#' head(fileIndex)
+#' head(metaIndex)
+#' head(geoIndex)
+#' 
+#' # in functions, you can use head(rdwd:::fileIndex) etc, but I don't export them
+#' # because Hadley says 'Never @export a data set' in
+#' # browseURL("http://r-pkgs.had.co.nz/data.html#data-data")
+#' 
+data(fileIndex, envir=environment())
+data(metaIndex, envir=environment())
+data( geoIndex, envir=environment())
+data(gridIndex, envir=environment())
+data(formatIndex, envir=environment())
+# http://stackoverflow.com/questions/32964741/accessing-sysdata-rda-within-package-functions
+# http://stackoverflow.com/questions/9521009/how-do-you-handle-r-data-internal-to-a-package
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
+# small helper functions, onload code, release questions
+# turn on Rstudio Outline (CTRL + SHIFT + O) for overview
+
+# * rdwdquiet ----
+
 #' @title global quiet option for rdwd
 #' @description global quiet option. The default `rdwdquiet()` is `FALSE`.\cr
 #' Just write the following in your code and all subsequent calls will be quiet:\cr
@@ -17,5 +22,92 @@ cv
 {
 options(rdwdquiet=FALSE)
 invisible(NULL)
+}
+
+
+
+# * release_questions ----
+
+#' @title Reminders when using devtools::release
+#' @description  Reminders when using devtools::release.
+#' @keywords internal
+
+release_questions <- function() {
+  c(
+    "Have you run updateIndexes() and runLocalTests()?"
+  )
+}
+
+
+
+# * dwdbase / gridbase ----
+
+#' @title DWD FTP Server base URL
+#' @aliases gridbase
+#' @export
+#' @description base URLs to the DWD FTP Server\cr\cr
+#' **`dwdbase`**: observed climatic records at\cr
+#' <ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate>\cr
+#' An overview of available datasets and usage suggestions can be found at\cr
+#' <https://bookdown.org/brry/rdwd/available-datasets.html>\cr
+#' <https://bookdown.org/brry/rdwd/station-selection.html>\cr\cr\cr
+#' **`gridbase`**: spatially interpolated gridded data at\cr
+#' <ftp://opendata.dwd.de/climate_environment/CDC/grids_germany>\cr
+#' Usage instructions can be found at\cr
+#' <https://bookdown.org/brry/rdwd/raster-data.html>
+#' 
+dwdbase <- "ftp://opendata.dwd.de/climate_environment/CDC/observations_germany/climate"
+#' @export
+gridbase <- "ftp://opendata.dwd.de/climate_environment/CDC/grids_germany"
+
+
+
+# * rowDisplay ---------------------------------------------------------------------
+
+#' Create leaflet map popup from data.frame rows
+#' 
+#' Create display character string for leaflet map popup from data.frame rows.
+#' This function is not exported, as it is only internally useful.
+#' A generic version is available in \code{berryFunctions::\link[berryFunctions]{popleaf}}.
+#' 
+#' @return Vector of character strings, one for each row in x.
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Feb 2017
+#' @seealso \code{\link{geoIndex}}
+#' @keywords character
+#' @importFrom berryFunctions removeSpace
+#' 
+#' @param x data.frame with colnames
+#' 
+rowDisplay <- function(
+x
+)
+{
+perrow <- function(x) paste0("rdwd::metaInfo(id=",removeSpace(x[1]),")<br>",
+                             paste0(names(x)[-1], ": ", x[-1], collapse="<br>"))
+apply(x, MARGIN=1, perrow)
+}
+
+
+
+# * checkSuggestedPackage --------------------------------------------------------
+
+#' @title check suggested package for availability
+#' @description check suggested package for availability,
+#'              yielding an instructive error message if not
+#' @return invisible success logical value from \code{\link{requireNamespace}}
+#' @author Berry Boessenkool, \email{berry-b@@gmx.de}, Aug 2019
+#' @seealso \code{\link{requireNamespace}}
+#' @keywords package
+# @export # only needed internally, I think
+#' 
+#' @param package      Charstring: package to be checked for loadability
+#' @param functionname Charstring: function name to be used in the message
+#' 
+checkSuggestedPackage <- function(package, functionname)
+{
+available <- requireNamespace(package, quietly=TRUE)
+if(!available) stop("To use ",functionname, ", please first install ",
+                    package,":    install.packages('",package,"')", call.=FALSE)
+return(invisible(available))
 }
 
