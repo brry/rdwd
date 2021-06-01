e781f8295933853ab99f1fee08485a0bff2848de
brry <berryboessenkool@hotmail.com>
2021-06-02 00:53:57 +0200

    validFileTypes: initial release

--- R/fileType.R
+++ R/fileType.R
@@ -1,3 +1,11 @@
+
+#' @title valid fileType values
+#' @description fileType values that have a reading subfunction `readDWD.ftype()`.
+#' @export
+validFileTypes <- strsplit("data,meta,multia,stand,deriv,radar,binary,raster,nc,asc,grib2", ",")[[1]]
+
+
+
 #' @title determine DWD file type
 #' @description determine which subfunction to call in [readDWD()] from the file extension (ext).\cr\cr
 #' The first block is for **observational data** ([overview](https://bookdown.org/brry/rdwd/available-datasets.html)),
@@ -42,15 +50,15 @@
 #' ft <- read.table(header=TRUE, stringsAsFactors=FALSE, text="
 #' type    filename
 #' data    daily_kl_recent_tageswerte_KL_03987_akt.zip
-#' stand   subdaily_standard_format_kl_10381_00_akt.txt
 #' meta    daily_kl_recent_KL_Tageswerte_Beschreibung_Stationen.txt
 #' multia  multi_annual_mean_81-10_Temperatur_1981-2010_aktStandort.txt
+#' stand   subdaily_standard_format_kl_10381_00_akt.txt
 #' deriv   derived_germany_soil_daily_historical_3987.txt.gz
 #' 
+#' radar   radolan_recent_bin_raa01-rw_10000-1802020250-dwd---bin.gz
 #' binary  daily_radolan_historical_bin_2017_SF201712.tar.gz
 #' raster  16_DJF_grids_germany_seasonal_air_temp_mean_188216.asc.gz
 #' nc      daily_Project_TRY_humidity_RH_199509_daymean.nc.gz
-#' radar   radolan_recent_bin_raa01-rw_10000-1802020250-dwd---bin.gz
 #' asc     radolan_historical_asc_2018_RW-201809.tar
 #' grib2   ftp_weather_nwp_cosmo-d2_005_T_2M.grib2.bz2
 #' ")
@@ -58,6 +66,9 @@
 #' 
 #' stopifnot(fileType(ft$filename)==ft$type)
 #' fileType("random_stuff.odt")
+#' 
+#' stopifnot(validFileTypes %in% ft$type)
+#' stopifnot(ft$type %in% validFileTypes)
 #'
 #' @param file Filename(s) with extension.
 #'
